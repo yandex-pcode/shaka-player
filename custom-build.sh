49d7dabe4eca9b4de5c60563a4c2f1abac11b99e
alex-gusev <alex-gusev@yandex-team.ru>
2019-10-31 14:51:28 +0300

    add custom build script

---
+++ custom-build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+rm -rf dist
+
+python build/gendeps.py
+python build/build.py +@complete \
+  -@hls -@ui -@cast -@offline \
+  -lib/polyfill/patchedmediakeys_apple.js \
+  -lib/polyfill/fullscreen.js \
+  -lib/polyfill/pip_webkit.js  \
+  --force --name compiled
+
+python build/build.py +@complete \
+  -@hls -@ui -@cast -@offline \
+  -lib/polyfill/patchedmediakeys_apple.js \
+  -lib/polyfill/fullscreen.js \
+  -lib/polyfill/pip_webkit.js  \
+  --force --name compiled --debug
