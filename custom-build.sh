d8dcc72e3b3e86f3fc62db5f4efe97afbafac1b8
alex-gusev <alex-gusev@yandex-team.ru>
2020-06-23 20:27:03 +0300

    use mp4box for runtime segmentation

--- custom-build.sh
+++ custom-build.sh
@@ -11,6 +11,12 @@ python build/build.py +@complete \
   -lib/polyfill/pip_webkit.js  \
   --force --name compiled
 
+node_modules/.bin/ncat \
+  node_modules/mp4box/dist/mp4box.all.min.js \
+  dist/shaka-player.compiled.js \
+  -m true \
+  -o dist/shaka-player.compiled.js
+
 python build/build.py +@complete \
   -@hls -@ui -@cast -@offline \
   -lib/polyfill/patchedmediakeys_apple.js \
@@ -18,3 +24,9 @@ python build/build.py +@complete \
   -lib/polyfill/fullscreen.js \
   -lib/polyfill/pip_webkit.js  \
   --force --name compiled --debug
+
+node_modules/.bin/ncat \
+  node_modules/mp4box/dist/mp4box.all.js \
+  dist/shaka-player.compiled.debug.js \
+  -m true \
+  -o dist/shaka-player.compiled.debug.js
